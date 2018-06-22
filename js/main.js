fe02fe40834b5c779aef15d86739226924c5894d
Ndifon Desmond Yong <malico.yong@gmail.com>
2018-06-22 23:30:05 +0100

    Scrolling JS

--- js/main.js
+++ js/main.js
@@ -11,6 +11,20 @@ function scroll(){
         header.classList.remove('float');
     }
 }
+// Checkit it's time to change image
+function chckImage(){
+    let img = document.querySelector(".bg-img");
+
+    let scTop = document.body.scrollTop % (2 * window.innerHeight);
+    let dc = document.documentElement.scrollTop % (2 * window.innerHeight);
+
+    if(document.body.scrollTop < scTop || document.documentElement.scrollTop > dc){
+        img.src = "/img/logo.png";
+    } else {
+        img.src = "/img/viva.png";
+    }
+}
 window.addEventListener("scroll", function(){
     scroll();
+    chckImage();
 });
